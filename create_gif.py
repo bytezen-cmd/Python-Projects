418201d615d54eed8b68f765efba4fd3369f2997
bytezen-cmd <bytepulse2025@outlook.com>
2025-11-26 22:19:44 +0530

    Completed Code

--- create_gif.py
+++ create_gif.py
@@ -1,3 +1,14 @@
 import imageio.v3 as iio
+from PIL import Image
 
-filename = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg']
+filenames = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg']
+images = []
+
+first = Image.open(filenames[0])
+w,h = first.size
+
+for filename in filenames:
+    img = Image.open(filename).resize((w,h))
+    images.append(img)
+
+iio.imwrite('cat_running.gif', images, duration=200, loop=0)
